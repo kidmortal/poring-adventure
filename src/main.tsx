76e63c4de18b879697f170daf68bb7952faa1d57
kidmortal <kidmortal@gmail.com>
2024-03-04 12:03:02 -0300

    add router file

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,6 @@
-import * as ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./firebase.ts";
-import "./styles/global.scss";
-import { HomePage } from "./pages/home/index.tsx";
-import { LimitedSizeLayout } from "./layout/LimitedSize/index.tsx";
-import { LoginPage } from "./pages/login/index.tsx";
-import { AuthLayout } from "./layout/Auth/index.tsx";
-import { PageLoadingLayout } from "./layout/PageLoading/index.tsx";
+import { createRoot } from "react-dom/client";
+import { Router } from "./router";
 
-const router = (
-  <BrowserRouter>
-    <Routes>
-      <Route element={<LimitedSizeLayout />}>
-        <Route element={<AuthLayout />}>
-          <Route element={<PageLoadingLayout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-          </Route>
-        </Route>
-      </Route>
-    </Routes>
-  </BrowserRouter>
-);
+const root = createRoot(document.getElementById("root")!);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(router);
+root.render(<Router />);
---
+++ src/router.tsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./firebase.ts";
+import "./styles/global.scss";
+import { HomePage } from "./pages/home/index.tsx";
+import { LimitedSizeLayout } from "./layout/LimitedSize/index.tsx";
+import { LoginPage } from "./pages/login/index.tsx";
+import { AuthLayout } from "./layout/Auth/index.tsx";
+import { PageLoadingLayout } from "./layout/PageLoading/index.tsx";
+
+export function Router() {
+  return (
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<LimitedSizeLayout />}>
+            <Route element={<AuthLayout />}>
+              <Route element={<PageLoadingLayout />}>
+                <Route path="/*" element={<HomePage />} />
+                <Route path="/login*" element={<LoginPage />} />
+              </Route>
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
+}
