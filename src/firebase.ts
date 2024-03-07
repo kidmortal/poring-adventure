e0ad6d0347dd5dec309dd2cd86a56e0fbbed1980
kidmortal <kidmortal@gmail.com>
2024-03-07 08:39:57 -0300

    yeh

---
+++ src/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "kidmortal.firebaseapp.com",
+  projectId: "kidmortal",
+  storageBucket: "kidmortal.appspot.com",
+  messagingSenderId: "596344810334",
+  appId: "1:596344810334:web:f63eb8d84f44b1df90bdcf",
+  measurementId: "G-20YVCZZSMC",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
