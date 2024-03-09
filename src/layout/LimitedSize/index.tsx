76ebd1d28a09bc98b333252ac1693badb2884fc7
kidmortal <kidmortal@gmail.com>
2024-03-09 13:06:39 -0300

    increase limited space

--- src/layout/LimitedSize/index.tsx
+++ src/layout/LimitedSize/index.tsx
@@ -2,11 +2,16 @@ import { Outlet } from "react-router-dom";
 import styles from "./style.module.scss";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import cn from "classnames";
 
 export function LimitedSizeLayout() {
   return (
     <div className={styles.container}>
-      <div className={styles.limitedContainer}>
+      <div
+        className={cn(styles.limitedContainer, {
+          [styles.limitedDev]: import.meta.env.DEV,
+        })}
+      >
         <ToastContainer />
         <Outlet />
       </div>
