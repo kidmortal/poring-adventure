d03bfc73eac1f4b04e960f495946fb13509bd542
kidmortal <kidmortal@gmail.com>
2024-03-15 10:33:29 -0300

    fix buggy svg

--- src/assets/Skillboook.tsx
+++ src/assets/Skillboook.tsx
@@ -1,8 +1,8 @@
 export function Skillbook() {
   return (
     <svg
-      width="800px"
-      height="800px"
+      width="25px"
+      height="25px"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
